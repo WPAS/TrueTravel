3ccaa9177e6856d12ce18ead97b10f22d47c8657
Wojciech Pastuszka <wojciech.pastuszka@gazeta.pl>
2016-11-14 17:01:32 +0100

    add header.scss and slider script

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 //navigation code for mobiles
+
 var button = document.querySelector("#nav-button");
 var navigation = document.querySelector(".menu");
 var hider = document.querySelector("#hider")
@@ -38,6 +39,31 @@ hider.addEventListener("click", function() {
   hider.classList.add("hidden");
 });
 
+//slider
+
+var btnPrev = document.querySelector("#gallery-nav1");
+var btnNext = document.querySelector("#gallery-nav2");
+var slides = document.querySelectorAll(".slider");
+var counter = 0;
+
+btnNext.addEventListener("click", function() {
+  slides[counter].classList.add("hidden")
+  counter += 1;
+  if (counter > slides.length -1) {
+    counter = 0;
+  }
+  slides[counter].classList.remove("hidden");
+});
+
+btnPrev.addEventListener("click", function() {
+  slides[counter].classList.add("hidden")
+  counter -= 1;
+  if (counter === -1) {
+    counter = slides.length -1;
+  }
+  slides[counter].classList.remove("hidden");
+
+});
 
 
 
